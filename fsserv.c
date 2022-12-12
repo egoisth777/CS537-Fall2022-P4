db96a45747b3853ce19629c015f68813011a9252
Billy Li <egoistheresy@outlook.com>
2022-12-12 17:21:48 -0600

    reconstruct write function

--- fsserv.c
+++ fsserv.c
@@ -256,6 +256,100 @@ rm_file(int inum, inode_t *inode_table, char* data_bitmap, char* inode_bitmap)
     return 0;
 }
 
+
+/**
+ * @brief Wrapper for the MFS write function in the server side 
+ * 
+ * @param nbytes 
+ * @param offset 
+ * @param inum 
+ * @param inode_table 
+ * @param data_bitmap 
+ * @param inode_bitmap 
+ * @param buffer 
+ * @param superBlock 
+ * @param image 
+ * @return int 
+ */
+int
+MFS_write(int nbytes, int offset, int inum, inode_t * inode_table, char * data_bitmap, char * inode_bitmap, char * buffer, super_t * superBlock, void * image){
+    // precheck
+    if (nbytes <= 0 || nbytes > BLOCK_SIZE || offset < 0 || offset + nbytes >= BLOCK_SIZE * DIRECT_PTRS)
+    {
+        return -1;
+    }
+    // Check if it is a regular file
+    inode_t metadata = inode_table[inum];
+    if (metadata.type == 0)
+    { // directory {}
+        return -1;
+    }
+
+    int numBlockToWrite = findNoBlockAlloc(offset, nbytes); // Number of Data Block To Write: 1 or 2
+    unsigned int comparison = -1;
+
+    int numByteToWriteFirstBlock = numBlockToWrite == 2 ? BLOCK_SIZE - offset : nbytes;
+    int startAddrFirstBlockOffset = offset % BLOCK_SIZE;
+    int numByteToWriteSecondBlock = numBlockToWrite == 1 ? 0 : nbytes - numByteToWriteFirstBlock;
+
+    int locationFirstBlock = offset / BLOCK_SIZE;
+    unsigned locationFirstBlockNum = metadata.direct[locationFirstBlock];
+    int locationSecondBlock = locationFirstBlock + 1; // prove this
+    unsigned locationSecondBlockNum = metadata.direct[locationSecondBlock];
+
+    // Operation on First Block
+    int firstBlockAllocated = locationFirstBlockNum == comparison ? 0 : 1;
+    int emptySlot;
+    if (firstBlockAllocated == 0)
+    {
+        firstBlockAllocated = find_empty_set_bitmap((unsigned int *)data_bitmap, superBlock->num_data, &emptySlot);
+    }
+    metadata.direct[locationFirstBlock] = emptySlot;
+    locationFirstBlockNum = metadata.direct[locationFirstBlock];
+
+    if (firstBlockAllocated == 0)
+    {
+        return -1;
+    }
+
+
+    char *startAddr = image + superBlock->data_region_addr + BLOCK_SIZE * locationFirstBlockNum + startAddrFirstBlockOffset;
+    // Write to persistency file
+    memcpy(startAddr, buffer, numByteToWriteFirstBlock);
+    msync(startAddr, numByteToWriteFirstBlock, MS_SYNC);
+
+    if (numBlockToWrite == 2)
+    {
+        // Operation on Second Block
+        int secondBlockAllocated = locationSecondBlockNum == comparison ? 0 : 1;
+        if (secondBlockAllocated == 0)
+        {
+            secondBlockAllocated = find_empty_set_bitmap((unsigned int *)data_bitmap, superBlock->num_data, &emptySlot);
+        }
+
+        metadata.direct[locationSecondBlock] = emptySlot;
+        locationSecondBlockNum = metadata.direct[locationSecondBlock];
+
+        if (secondBlockAllocated == 0)
+        {
+            return -1;
+        }
+        
+        char *startAddr2 = image + superBlock->data_region_addr + BLOCK_SIZE * locationSecondBlockNum;
+        // Write to persistency file
+        memcpy(startAddr2, buffer, numByteToWriteSecondBlock);
+        msync(startAddr2, numByteToWriteSecondBlock, MS_SYNC);
+    }
+
+    // update the size accordingly
+    metadata.size = (nbytes + offset) > metadata.size ? nbytes + offset : metadata.size;
+
+    msync(inode_bitmap, superBlock->inode_bitmap_len * BLOCK_SIZE, MS_SYNC);
+    msync(data_bitmap, superBlock->data_bitmap_len * BLOCK_SIZE, MS_SYNC);
+    return 0;
+}
+
+
 int main(int argc, char const *argv[])
 {
     // Get length of the argument
@@ -331,6 +425,9 @@ int main(int argc, char const *argv[])
         }
 
         message reply_msg; // message to be replied to client
+
+
+        
         char *msg = received_msg.msg;
         char *buffer = received_msg.buf;
         int param1 = received_msg.param1; // pinum/inum
@@ -354,15 +451,13 @@ int main(int argc, char const *argv[])
             int pinum = param1;
             char *name = received_msg.charParam;
             int inum;
-
             int found = lookup(pinum, name, inode_table, data_region, &inum, superBlock->data_region_addr);
-            found = -1 ? found == 0 : inum;
+            found = found == -1 ? -1 : inum;
             respondToServer(reply_msg, found, sd, &addr, &rc);
         }
         else if (strcmp(msg, "MFS_Stat") == 0)
         {
-            int inum = param1;
-            inode_t metadata = inode_table[inum];
+            inode_t metadata = inode_table[param1];
             reply_msg.param1 = metadata.size; // size of the inode
             reply_msg.param2 = metadata.type; // type of the inode
             respondToServer(reply_msg, 0, sd, &addr, &rc);
@@ -373,77 +468,8 @@ int main(int argc, char const *argv[])
             int offset = param2;
             int nbytes = param3;
 
-            if (nbytes <= 0 || nbytes > BLOCK_SIZE || offset < 0 || offset + nbytes >= BLOCK_SIZE * DIRECT_PTRS)
-            {
-                respondToServer(reply_msg, -1, sd, &addr, &rc);
-                continue;
-            }
-            // Check if it is a regular file
-            inode_t metadata = inode_table[inum];
-            if (metadata.type == 0) { // directory {}
-                respondToServer(reply_msg, -1, sd, &addr, &rc);
-                continue;
-            }
-            int numBlockToWrite = findNoBlockAlloc(offset, nbytes); // Number of Data Block To Write: 1 or 2
-            unsigned int comparison = -1;
-
-            int numByteToWriteFirstBlock = numBlockToWrite == 2 ? BLOCK_SIZE - offset : nbytes;
-            int startAddrFirstBlockOffset = nbytes % BLOCK_SIZE;
-            int numByteToWriteSecondBlock = numBlockToWrite == 1 ? 0 : nbytes - numByteToWriteFirstBlock;
-
-            int locationFirstBlock = offset / BLOCK_SIZE;
-            unsigned locationFirstBlockNum = metadata.direct[locationFirstBlock];
-            int locationSecondBlock = locationFirstBlock + 1; // prove this
-            unsigned locationSecondBlockNum = metadata.direct[locationSecondBlock];
-
-            // Operation on First Block
-            int firstBlockAllocated = locationFirstBlockNum == comparison ? 0 : 1;
-            int *emptySlot;
-            if (firstBlockAllocated == 0)
-            {
-                firstBlockAllocated = find_empty_set_bitmap((unsigned int *) data_bitmap, superBlock->num_data, emptySlot);
-            }
-            locationFirstBlockNum = metadata.direct[locationFirstBlock];
-
-            if (firstBlockAllocated == 0)
-            {
-                respondToServer(reply_msg, -1, sd, &addr, &rc);
-                continue;
-            }
-
-            metadata.direct[locationFirstBlock] = *emptySlot;
-
-            char *startAddr = image + superBlock->data_region_addr + BLOCK_SIZE * locationFirstBlockNum + startAddrFirstBlockOffset;
-            // Write to persistency file
-            memcpy(startAddr, buffer, numByteToWriteFirstBlock);
-            msync(startAddr, numByteToWriteFirstBlock, MS_SYNC);
-
-            if (numBlockToWrite == 2)
-            {
-                // Operation on Second Block
-                int secondBlockAllocated = locationSecondBlockNum == comparison ? 0 : 1;
-                if (secondBlockAllocated == 0)
-                {
-                    secondBlockAllocated = find_empty_set_bitmap((unsigned int *) data_bitmap, superBlock->num_data, emptySlot);
-                }
-                locationSecondBlockNum = metadata.direct[locationSecondBlock];
-
-                if (secondBlockAllocated == 0)
-                    respondToServer(reply_msg, -1, sd, &addr, &rc);
-
-                metadata.direct[locationSecondBlock] = *emptySlot;
-                char *startAddr2 = image + superBlock->data_region_addr + BLOCK_SIZE * locationSecondBlockNum;
-                // Write to persistency file
-                memcpy(startAddr2, buffer, numByteToWriteSecondBlock);
-                msync(startAddr2, numByteToWriteSecondBlock, MS_SYNC);
-            }
-
-            // update the size accordingly
-            metadata.size = (nbytes + offset) > metadata.size? nbytes + offset : metadata.size;
-
-            msync(inode_bitmap, superBlock->inode_bitmap_len * BLOCK_SIZE, MS_SYNC);
-            msync(data_bitmap, superBlock->data_bitmap_len * BLOCK_SIZE, MS_SYNC);
-            respondToServer(reply_msg, 0, sd, &addr, &rc);
+            int res = MFS_write(nbytes, offset, inum, inode_table, data_bitmap, inode_bitmap, buffer, superBlock, image);
+            respondToServer(reply_msg, res, sd, &addr, &rc);
         }
         else if (strcmp(msg, "MFS_Read") == 0)
         {
